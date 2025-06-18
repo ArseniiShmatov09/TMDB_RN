5233bb44b662418338e161223968c09719078e03
Арсений Шматов <arseniishmatov@MacBook-Pro-Arsenij.local>
2025-06-18 13:06:08 +0300

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, useColorScheme, View, Text } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+     <Text>ddd</Text>
     </View>
   );
 }
